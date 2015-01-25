f77b5968afec02dea0c99f159c8e7fa9d3241fd5
Paul Wickett <paul.wickett@gmail.com>
2015-01-25 16:19:58 -0500

    Updated Functions

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,1 +1,11 @@
-cacheSolve<-function()
+cacheSolve<-function(x, ...) {
+        I<-x$getinverse()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        matix<-x$get
+        I<-solve(matrix,...)
+        x$setinverse(I)
+        I
+}
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,1 +1,13 @@
-makeCacheMatrix<-function
+makeCacheMatrix<-function(x = numeric()){
+        I<-NULL
+        set<-function(y){
+                x<<-y
+                I<<-NULL
+        }
+        get<-function() x
+        setinverse<-function(solve) I<<-solve
+        getinverse<-function() I
+        list(set=set,get=get),
+                setinverse=setinverse,
+                getinverse=getinverse)
+}
